a1aa760b1e4bce8af597d9eddf49a211d3279ed3
JeS24 <jyotirmaya.shivottam@niser.ac.in>
2024-08-05 18:53:00 +0530

    Updated julia script

--- scripts/julia-scicomp.jl
+++ scripts/julia-scicomp.jl
@@ -1,8 +1,35 @@
 # julia-scicomp.jl
 import Pkg
 
-# Include base utilities
-Pkg.activate("../julia-baseutils")
+# Essential utility packages
+Pkg.add("OhMyREPL")
+Pkg.add("Term")
+Pkg.add("UnicodePlots")
+Pkg.add("Markdown")
+Pkg.add("ProgressMeter")
+Pkg.add("Revise")
+Pkg.add("BenchmarkTools")
+Pkg.add("PrecompileTools")
+Pkg.add("PackageCompiler")
+Pkg.add("Documenter")
+Pkg.add("JSON")
+
+# Notebooks and interactive environments
+Pkg.add("Pluto")
+Pkg.add("IJulia")
+
+# Visualization packages
+Pkg.add("Plots")
+Pkg.add("Makie")
+Pkg.add("Gadfly")
+Pkg.add("Javis")
+
+# Machine learning packages
+Pkg.add("Flux")
+Pkg.add("Zygote")
+# Pkg.add("GraphNeuralNetworks") # If you are interested in GNNs
+Pkg.add("CUDA")  # If GPU is available
+Pkg.add("Transformers")
 
 # Scientific computing packages
 Pkg.add("DataFrames")
